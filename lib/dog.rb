0f7e07c4e97e18d9da493178222979f85687be3c
Beth Ruff <beth.ruff2@gmail.com>
2020-02-11 05:41:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog 
   
   @@all = []
+  @@clear_all = []
   
   attr_accessor :name 
   
@@ -12,6 +13,10 @@ class Dog
   def self.all 
     @@all
   end
+  
+  def self.clear_all
+    @@clear_all
+  end 
   
 end
 
